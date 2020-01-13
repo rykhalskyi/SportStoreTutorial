3b7cd8f249fcf652db9d6275a79342cb7d582d2e
rykhalskyi <rikhalskiy@gmail.com>
2020-01-13 19:41:44 +0200

    Complete OrderController

--- SportStore/SportStore/Controllers/OrderController.cs
+++ SportStore/SportStore/Controllers/OrderController.cs
@@ -1,10 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportStore.Models;
+using System.Linq;
 
 namespace SportStore.Controllers
 {
     public class OrderController : Controller
     {
+        IOrderRepository _repository;
+        Cart _cart;
+
+        public OrderController(IOrderRepository repository, Cart cart)
+        {
+            _repository = repository;
+            _cart = cart;
+        }
+
         public ViewResult Checkout() => View(new Order());
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (_cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty");
+            }
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = _cart.Lines.ToArray();
+                _repository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        public ViewResult Completed()
+        {
+            _cart.Clear();
+            return View();
+        }
     }
+
 }
